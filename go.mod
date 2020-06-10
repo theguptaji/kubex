209e476318efe27d215398c8bf99b0f485fb7582
Aman Gupta <aman@keio.jp>
2020-06-10 18:18:17 +0900

    WIP

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/theguptaji/kubex
 
 go 1.14
+
+require github.com/urfave/cli/v2 v2.2.0
--- src/main.go
+++ src/main.go
@@ -1,9 +1,25 @@
 package main
 
-func main() {
-	kubex()
-}
+import (
+"fmt"
+"log"
+"os"
 
-func kubex() string {
-	return "Implement me Senpai!!!"
+"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name: "boom",
+		Usage: "make an explosive entrance",
+		Action: func(c *cli.Context) error {
+			fmt.Println("boom! I say!")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
