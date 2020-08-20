84f92d48d171066c6f2f4d1c550309ee03faca54
Aman Gupta <aman@keio.jp>
2020-08-20 14:45:52 +0900

    setup urfave/cli/2 and basic commands structure

--- src/main.go
+++ src/main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
-"fmt"
-"log"
-"os"
+	"fmt"
+	"log"
+	"os"
 
-"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	app := &cli.App{
-		Name: "boom",
-		Usage: "make an explosive entrance",
-		Action: func(c *cli.Context) error {
-			fmt.Println("boom! I say!")
-			return nil
+		Commands: []*cli.Command{
+			{
+				Name:    "load",
+				Aliases: []string{"l"},
+				Usage:   "load a project into kubex",
+				Action:  loadProject,
+			},
+			{
+				Name:    "unload",
+				Aliases: []string{"u"},
+				Usage:   "unload this project from kubex",
+				Action:  unloadProject,
+			},
+			{
+				Name:    "build",
+				Aliases: []string{"b"},
+				Usage:   "build the manifests for the current project",
+				Action:  buildProject,
+			},
+			{
+				Name:    "show",
+				Aliases: []string{"s"},
+				Usage:   "show the network flow for the current project",
+				Action:  showProject,
+			},
 		},
 	}
 
@@ -22,4 +42,24 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func loadProject(c *cli.Context) error {
+	fmt.Println("Loading project:", c.Args().First())
+	return nil
+}
+
+func unloadProject(c *cli.Context) error {
+	fmt.Println("Unloading project:", c.Args().First())
+	return nil
+}
+
+func buildProject(c *cli.Context) error {
+	fmt.Println("Building project:", c.Args().First())
+	return nil
+}
+
+func showProject(c *cli.Context) error {
+	fmt.Println("Showing project:", c.Args().First())
+	return nil
 }
--- src/main_test.go
+++ src/main_test.go
@@ -1,12 +1,54 @@
 package main
 
-import "testing"
+import (
+	"os/exec"
+	"testing"
+)
 
-func Testkubex(t *testing.T) {
-	str := "Implement me Senpai!!!"
-
-	got := kubex()
-	if got != str {
-		t.Errorf("kubex() = %s; want %s", got, str)
+func TestLoadProject(t *testing.T) {
+	expected := "Loading project: hoge\n"
+	out, err := exec.Command("go", "run", "main.go", "load", "hoge").Output()
+	if err != nil {
+		t.Errorf("error while running command: %s", err)
 	}
+	if string(out) != expected {
+		t.Errorf("expected %s ; getting %s", expected, string(out))
+	}
+	return
+}
+
+func TestUnloadProject(t *testing.T) {
+	expected := "Unloading project: hoge\n"
+	out, err := exec.Command("go", "run", "main.go", "unload", "hoge").Output()
+	if err != nil {
+		t.Errorf("error while running command: %s", err)
+	}
+	if string(out) != expected {
+		t.Errorf("expected %s ; getting %s", expected, string(out))
+	}
+	return
+}
+
+func TestBuildProject(t *testing.T) {
+	expected := "Building project: hoge\n"
+	out, err := exec.Command("go", "run", "main.go", "build", "hoge").Output()
+	if err != nil {
+		t.Errorf("error while running command: %s", err)
+	}
+	if string(out) != expected {
+		t.Errorf("expected %s ; getting %s", expected, string(out))
+	}
+	return
+}
+
+func TestShowProject(t *testing.T) {
+	expected := "Showing project: hoge\n"
+	out, err := exec.Command("go", "run", "main.go", "show", "hoge").Output()
+	if err != nil {
+		t.Errorf("error while running command: %s", err)
+	}
+	if string(out) != expected {
+		t.Errorf("expected %s ; getting %s", expected, string(out))
+	}
+	return
 }
